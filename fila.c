a7891fa6dde112a3d962a8bf1560d149044bf2bb
Mauricio Catanio <mauricatanio@yahoo.com.br>
2016-08-23 12:04:23 -0300

    Update fila.c

--- fila.c
+++ fila.c
@@ -18,41 +18,43 @@ void startFila(Fila *nomeDaFila){
 }
 
 //checa se está vazia
-int emptyCheck(Fila nomeDaFila){
-	if(nomeDaFila.fim==NULL)
+int emptyCheck(Fila *nomeDaFila){
+	if(nomeDaFila->fim==NULL)
 		return 1; //A fila está vazia
 	else return 0;
 }
 
-void filaPop(Fila *fila, int dado){
-	Node *temp=fila->fim;//se só tiver um elemento na fila, tem que tratar (segmentation fault)
-	if(temp->next=NULL)
-	{
-		fila->fim=NULL;
-		free(temp);
-		return;
-	}
-	while(temp->next->next!=NULL){
-		temp=tempnext;
-	}//implementa no for
-	free(temp->next);
-	temp->next=NULL;
-}	
-
 void filaPush(Fila *fila, int dado){
-	Node *noh;//criou o apontamento
-	noh=(Node*)malloc(sizeof(Node));//cria a estrutura a partir do endereço
-	noh.data=dado; 
+	Node *noh=(Node*)malloc(sizeof(Node));//criou o apontamento
+	noh->data=dado; 
 	if (emptyCheck(fila)==1){
-		fila->fim=&noh;
+		fila->fim=noh;
 	}else{
 		noh->next=fila->fim;
 		fila->fim=noh;
 	}
 }
 
-int main(argc, argv){
+
+void filaPop(Fila *fila, int dado){
+	Node *temp=fila->fim, *ant;//se só tiver um elemento na fila, tem que tratar (segmentation fault)
+	if(temp->next=NULL)
+	{
+		fila->fim=NULL;
+		free(temp);
+		return;
+	}
+	while(temp->next!=NULL){
+		ant=temp;		
+		temp=temp->next;
+	}//implementa no for
 	
+	free(temp);
+	ant->next=NULL;
+}	
+
+
+int main(argc, argv){
 	
 	return 0;
 }
