02a4fcc8360127c9ebd030485d6cc67a14e9430f
Mauricio Catanio <mauricatanio@yahoo.com.br>
2016-10-13 17:24:28 -0300

    Update InsertionSort.c

--- InsertionSort.c
+++ InsertionSort.c
@@ -14,7 +14,7 @@ int main () {
 		scanf("%d", &sel);
 		//Insertion em vetor
 		if (sel == 1 ) {
-			int *vetor;
+			int *vetor, atual;
 			printf("quantas posições o vetor tem de ter?");
 			scanf("%d", &tamanho);
 
@@ -23,14 +23,24 @@ int main () {
 
 			//preenche com valores aleatórios
 			for (i=0; i<tamanho; i++)
-				vetor[i]=rand();%500;
+				vetor[i]=rand()%500;
 
 			//mostra vetor embaralhado 
 			printf("vetor embaralhado: %d", vetor[0]);
 			for (i=1; i<tamanho; i++)
 				printf(",%d ", vetor[i]);
 
-			
+			//faz o insertion sort
+
+			for (i = 1 ; i < tamanho ; i ++ ) {
+				atual=vetor[i];
+				j=i-1;
+				while (atual > vetor[j]) {
+					vetor[j+1]=vetor[j];
+					j--;
+				}
+				vetor[j+1]=atual;
+			}
 			
 		}//lista
 		else if (sel == 2 ) {
