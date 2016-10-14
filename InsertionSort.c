61ee3f136681be2286a77f246c98849cd4cbe404
Mauricio Catanio <mauricatanio@yahoo.com.br>
2016-10-14 08:15:01 -0300

    Update InsertionSort.c

--- InsertionSort.c
+++ InsertionSort.c
@@ -10,20 +10,20 @@ int main () {
 	int i, j, sel, tamanho;
 	srand( (unsigned)time(NULL) );
 	do{
-		printf("insertion sort com: \n1 - Vetor\n2 - Lista");
+		printf("insertion sort com: \n1 - Vetor\n2 - Lista: ");
 		scanf("%d", &sel);
 		//Insertion em vetor
 		if (sel == 1 ) {
 			int *vetor, atual;
-			printf("quantas posições o vetor tem de ter?");
+			printf("quantas posições o vetor tem de ter? ");
 			scanf("%d", &tamanho);
 
 			//cria vetor no tamanho desejado
 			vetor = (int *) malloc (tamanho*sizeof(int));
-
+			printf("ué\n");
 			//preenche com valores aleatórios
 			for (i=0; i<tamanho; i++)
-				vetor[i]=rand()%500;
+				vetor[i]=(rand()%(tamanho*tamanho));
 
 			//mostra vetor embaralhado 
 			printf("vetor embaralhado: %d", vetor[0]);
@@ -32,16 +32,19 @@ int main () {
 
 			//faz o insertion sort
 
-			for (i = 1 ; i < tamanho ; i ++ ) {
+			for (i = 0 ; i < tamanho ; i ++ ) {
 				atual=vetor[i];
 				j=i-1;
-				while (atual > vetor[j]) {
+				while (j >= 0 && vetor[j] > atual) {
 					vetor[j+1]=vetor[j];
 					j--;
 				}
 				vetor[j+1]=atual;
 			}
-			
+			printf("\nvetor ordenado: %d", vetor[0]);
+			for (i=1; i<tamanho; i++)
+				printf(",%d ", vetor[i]);
+			printf("\n");
 		}//lista
 		else if (sel == 2 ) {
 
